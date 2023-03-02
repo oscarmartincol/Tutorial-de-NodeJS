28f338f2620fbda57697666a51f0358fb9b82585
Oscar Martinez <oscarmartincol@outlook.com>
2023-03-02 12:08:52 -0500

    Ruta que muestra el texto Eres genial agregada.

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/*GET Eres genial*/ 
+router.get('/cool/', function(req, res) {
+  res.send('Eres genial');
+});
+
 module.exports = router;
